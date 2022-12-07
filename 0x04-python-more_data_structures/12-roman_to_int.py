2f5f8c3300e422e5ea5cc7eaaadad25b7701cace
Afolayan Tolulope <afolayantoluwalope@gmail.com>
2022-12-07 19:31:30 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,45 +1,36 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    # Fail checks, none, not a string
-    if not roman_string:
-        return 0
     if not isinstance(roman_string, str):
         return 0
-    if not roman_string.isupper():
-        return 0
-    # Dictionary for roman numerals
-    r_dict = {
-        "I": 1,
-        "IV": 4,
-        "V": 5,
-        "IX": 9,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
+    num = 0
+    res = 0
+    aux = 0
+    list_num = []
 
-    result = 0
-    temp = list(roman_string)
-    # Concat 4 and 9s
-    if len(temp) > 1:
-        idx = 0
-        for i in temp:
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'V':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'X':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            idx += 1
-    # Search in dict for correct numbers and add
-    for k, v in r_dict.items():
-        for index in temp:
-            if index == k:
-                result += v
-    return result
+    for s in roman_string:
+        if s == 'I':
+            num = 1
+        elif s == 'V':
+            num = 5
+        elif s == 'X':
+            num = 10
+        elif s == 'L':
+            num = 50
+        elif s == 'C':
+            num = 100
+        elif s == 'D':
+            num = 500
+        elif s == 'M':
+            num = 1000
+        list_num.append(num)
+
+    len_list = len(list_num)
+    if len_list > 1:
+        for i in range(1, len_list):
+            if list_num[i] > list_num[i - 1]:
+                    aux = list_num[i] - list_num[i - 1]
+                    list_num[i] = aux
+                    list_num[i - 1] = 0
+    for num in list_num:
+        res += num
+    return 
