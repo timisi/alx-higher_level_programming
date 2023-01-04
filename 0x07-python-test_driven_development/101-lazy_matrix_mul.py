dcfa45f2f1f39f441e7fb881358633fd74a8f334
Afolayan Tolulope <afolayantoluwalope@gmail.com>
2023-01-04 20:06:08 +0100

    Update 101-lazy_matrix_mul.py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,6 +5,7 @@ This module supplies one function, lazy_matrix_mul().
 """
 import numpy as np
 
+
 def lazy_matrix_mul(m_a, m_b):
     """
     Return a new matrix where each element has been divided by div.
@@ -12,4 +13,5 @@ def lazy_matrix_mul(m_a, m_b):
         m_a (list): list of lists of integers or floats.
         m_b (list): list of lists of integers or floats.
     """
+
     return np.dot(m_a, m_b)
