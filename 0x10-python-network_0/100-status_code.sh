2445a5b8554a068f5f2039bd1e395aa812f50cc4
Afolayan Toluwalope <25456358+timisi@users.noreply.github.com>
2023-03-31 09:40:21 +0100

    New project

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that sends a request to a URL passed as an argument
-curl -o /dev/null -sw "%{http_code}" "$1"
+# Sends a GET request to a given URL and display the response status code.
+curl -s -o /dev/null -w "%{http_code}" "$1"
